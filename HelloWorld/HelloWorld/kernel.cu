b090643442133efe879714a004db7f300358504d
mJaJksJ <maksim.m00@mail.ru>
2022-09-20 12:18:33 +0300

    chore: доработка hello_world
    запуск на всех мультипроцессорах

--- HelloWorld/HelloWorld/kernel.cu
+++ HelloWorld/HelloWorld/kernel.cu
@@ -8,14 +8,14 @@
 
 __global__ void kernel(float* data)
 {
-	int idx = blockIdx.x * blockDim.x + threadIdx.x;
-	float x = 2 * 3.1415926 * static_cast<float>(idx) / static_cast<float>(N);
+	const int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	const float x = 2 * static_cast<float>(3.1415926) * static_cast<float>(idx) / static_cast<float>(N);
 	data[idx] = sinf(sqrtf(x));
 }
 
 void hello_world()
 {
-	auto a = static_cast<float*>(malloc(N * sizeof(float)));
+	const auto a = static_cast<float*>(malloc(N * sizeof(float)));
 	float* dev = nullptr;
 	// выделить память на GPU
 	cudaMalloc(reinterpret_cast<void**>(&dev), N * sizeof(float));
@@ -34,31 +34,32 @@ void hello_world()
 
 void about_devices()
 {
-	int deviceCount;
-	cudaDeviceProp devProp{};
-	cudaGetDeviceCount(&deviceCount);
-	printf("Found %d devices\n", deviceCount);
-	for (int device = 0; device < deviceCount; device++)
+	int device_count;
+	cudaDeviceProp dev_prop{};
+	cudaGetDeviceCount(&device_count);
+	printf("Found %d devices\n", device_count);
+	for (int device = 0; device < device_count; device++)
 	{
-		cudaGetDeviceProperties(&devProp, device);
+		cudaGetDeviceProperties(&dev_prop, device);
 		printf("Device %d\n", device);
-		printf("Compute capability : %d.%d\n", devProp.major, devProp.minor);
-		printf("Name : %s\n", devProp.name);
-		printf("Total Global Memory : %llu\n", devProp.totalGlobalMem);
-		printf("Shared memory per block: %llu\n", devProp.sharedMemPerBlock);
-		printf("Registers per block : %d\n", devProp.regsPerBlock);
-		printf("Warp size : %d\n", devProp.warpSize);
-		printf("Max threads per block : %d\n", devProp.maxThreadsPerBlock);
-		printf("Total constant memory : %llu\n", devProp.totalConstMem);
+		printf("Compute capability : %d.%d\n", dev_prop.major, dev_prop.minor);
+		printf("Name : %s\n", dev_prop.name);
+		printf("Total Global Memory : %llu\n", dev_prop.totalGlobalMem);
+		printf("Shared memory per block: %llu\n", dev_prop.sharedMemPerBlock);
+		printf("Registers per block : %d\n", dev_prop.regsPerBlock);
+		printf("Warp size : %d\n", dev_prop.warpSize);
+		printf("Max threads per block : %d\n", dev_prop.maxThreadsPerBlock);
+		printf("Total constant memory : %llu\n", dev_prop.totalConstMem);
+		printf("SM count: %d\n", dev_prop.multiProcessorCount);
 	}
 }
 
-void time_tracker()
+void time_tracker(bool default_blocks_count)
 {
 	//описываем переменные типа cudaEvent_t
 	cudaEvent_t start, stop;
 
-	float gpuTime = 0.0f;
+	float gpu_time = 0.0f;
 
 	// создаем события начала и окончания выполнения ядра
 	cudaEventCreate(&start);
@@ -68,18 +69,25 @@ void time_tracker()
 	cudaMalloc(reinterpret_cast<void**>(&dev), N * sizeof(float));
 
 	//привязываем событие start к данному месту
-	cudaEventRecord(start, 0);
+	cudaEventRecord(start, nullptr);
 	// вызвать ядро
-	kernel << <dim3((N / 512), 1), dim3(512, 1) >> > (dev);
+	if (default_blocks_count)
+	{
+		kernel << <dim3((N / 512), 1), dim3(512, 1) >> > (dev);
+	}
+	else
+	{
+		kernel << <dim3(16, 1), dim3(512, 1) >> > (dev);
+	}
 	//привязываем событие stop к данному месту
-	cudaEventRecord(stop, 0);
+	cudaEventRecord(stop, nullptr);
 	cudaEventSynchronize(stop);
 
 	cudaFree(dev);
 
 	// запрашиваем время между событиями
-	cudaEventElapsedTime(&gpuTime, start, stop);
-	printf("time spent executing by the GPU: %.5f ms\n", gpuTime);
+	cudaEventElapsedTime(&gpu_time, start, stop);
+	printf("time spent executing by the GPU (%d blocks): %.5f ms\n", default_blocks_count ? (N / 512) : 16, static_cast<double>(gpu_time));
 	// уничтожаем созданные события
 	cudaEventDestroy(start);
 	cudaEventDestroy(stop);
@@ -91,7 +99,8 @@ int main(int argc, char* argv[])
 
 	system("pause");
 
-	time_tracker();
+	time_tracker(true);
+	time_tracker(false);
 
 	system("pause");
 
